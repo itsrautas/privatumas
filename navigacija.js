59623d80e37fa006332033dbc3834d45ae9f0714
itsrautas <146126974+itsrautas@users.noreply.github.com>
2023-10-16 00:03:42 +0300

    Update navigacija.js

--- navigacija.js
+++ navigacija.js
@@ -223,7 +223,7 @@ document.getElementById("navigacija").innerHTML =
   
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 3c5.5 0 10 3.58 10 8s-4.5 8-10 8c-1.24 0-2.43-.18-3.53-.5C5.55 21 2 21 2 21c2.33-2.33 2.7-3.9 2.75-4.5C3.05 15.07 2 13.13 2 11c0-4.42 4.5-8 10-8m5 9v-2h-2v2h2m-4 0v-2h-2v2h2m-4 0v-2H7v2h2Z"/></svg>'+
   
-  '<span class="md-ellipsis">Paskyros ištrynimas</span>'+
+  '<span class="md-ellipsis">Paskyrų ištrynimas</span>'+
   
 
       '</a>'+
@@ -298,12 +298,12 @@ document.getElementById("navigacija").innerHTML =
   
   
     '<li class="md-nav__item">'+
-      '<a href="https://itsrautas.github.io/privatumas/kategorija/finansai.html" class="md-nav__link">'+
+      '<a href="https://itsrautas.github.io/privatumas/kategorija/kripto-valiutos.html" class="md-nav__link">'+
         
   
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.5 1 2 6v2h19V6m-5 4v7h3v-7M2 22h19v-3H2m8-9v7h3v-7m-9 0v7h3v-7H4Z"/></svg>'+
   
-  '<span class="md-ellipsis">Finansinės paslaugos</span>'+
+  '<span class="md-ellipsis">Kripto valiutos</span>'+
   
   
       '</a>'+
