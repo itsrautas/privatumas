35b47a523f59760271890843b64609fa3f90490c
itsrautas <146126974+itsrautas@users.noreply.github.com>
2023-10-18 23:54:21 +0300

    Update navigacija.js

--- navigacija.js
+++ navigacija.js
@@ -221,7 +221,7 @@ document.getElementById("navigacija").innerHTML =
       '<a href="https://itsrautas.github.io/privatumas/kategorija/paskyros.html" class="md-nav__link">'+
         
   
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 3c5.5 0 10 3.58 10 8s-4.5 8-10 8c-1.24 0-2.43-.18-3.53-.5C5.55 21 2 21 2 21c2.33-2.33 2.7-3.9 2.75-4.5C3.05 15.07 2 13.13 2 11c0-4.42 4.5-8 10-8m5 9v-2h-2v2h2m-4 0v-2h-2v2h2m-4 0v-2H7v2h2Z"/></svg>'+
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15 14c2.67 0 8 1.33 8 4v2H7v-2c0-2.67 5.33-4 8-4m0-2a4 4 0 0 1-4-4 4 4 0 0 1 4-4 4 4 0 0 1 4 4 4 4 0 0 1-4 4M5 9.59l2.12-2.13 1.42 1.42L6.41 11l2.13 2.12-1.42 1.42L5 12.41l-2.12 2.13-1.42-1.42L3.59 11 1.46 8.88l1.42-1.42L5 9.59Z"/></svg>'+
   
   '<span class="md-ellipsis">Paskyrų ištrynimas</span>'+
   
